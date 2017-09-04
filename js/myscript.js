f7d9dd4a7ebe9bdd9e68c393e1baa56a2084fd03
NoSchool <31620915+NoSchool@users.noreply.github.com>
2017-09-04 16:01:23 +0300

    Create myscript.js

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+/* Функция показывает и скрывает два блока, имена которых получает в параметрах вызова
+*/
+function change_visibility (block_4_close, block_4_open) {
+    document.getElementById(block_4_close).style.display='none';
+    document.getElementById(block_4_open).style.display='';
+}
